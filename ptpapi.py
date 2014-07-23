6b5446fbbe28c5cac4c06fc4332340b88f7f4371
kannibalox <kannibalox@gmail.com>
2014-07-23 10:31:11 -0400

    Add TokenBucket for eventual request limiter

--- ptpapi.py
+++ ptpapi.py
@@ -22,6 +22,35 @@ cookiesFile = 'cookies.txt'
 class PTPAPIException(Exception):
     pass
 
+class TokenBucket(object):
+    def __init__(self, tokens, fill_rate):
+        """tokens is the total tokens in the bucket. fill_rate is the
+        rate in tokens/second that the bucket will be refilled."""
+        self.capacity = float(tokens)
+        self._tokens = float(tokens)
+        self.fill_rate = float(fill_rate)
+        self.timestamp = time()
+        
+    def consume(self, tokens):
+        """Consume tokens from the bucket. Returns True if there were
+        sufficient tokens otherwise False."""
+        self.get_tokens()
+        if tokens <= self.tokens:
+            self._tokens -= tokens
+        else:
+            return False
+        return True
+
+    def get_tokens(self):
+        if self._tokens < self.capacity:
+            now = time()
+            delta = self.fill_rate * (now - self.timestamp)
+            self._tokens = min(self.capacity, self._tokens + delta)
+            self.timestamp = now
+        return self._tokens
+        
+    tokens = property(get_tokens)
+    
 class Movie:
     def __init__(self, ID=None, data=None):
         # Expects either a groupd ID to load data from, or for the data to already be present
