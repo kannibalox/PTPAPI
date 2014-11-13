6e90d654910644fb296cb27ca4806a65ef72137a
kannibalox <kannibalox@gmail.com>
2014-11-13 13:44:03 -0500

    Add start of need for seed script

---
+++ scripts/needforseed.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Search for torrents from other sites to reseed on ptp
+
+import argparse
+
+import ptpapi
+
+def main():
+    ptp = ptpapi.login(conf="creds.ini")
+    torrents = ptp.need_for_seed()
+
+if __name__ == '__main__':
+    main()
