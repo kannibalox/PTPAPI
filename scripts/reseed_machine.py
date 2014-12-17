443b9b20df96e8229a279fd76155b7e281fe9f03
kannibalox <kannibalox@gmail.com>
2014-12-16 20:22:20 -0500

    Change reseed_machine to work from file and record seen

--- scripts/reseed_machine.py
+++ scripts/reseed_machine.py
@@ -2,6 +2,7 @@ import sys
 import re
 import logging
 import readline
+import pickle
 from urlparse import urlparse, parse_qs
 
 import ptpapi
@@ -22,6 +23,7 @@ def findByURL(cg, URL):
     m.load_html_data()
     if 'ImdbId' not in m.data:
         logger.error('Movie has no IMDB id, cannot lookup')
+        return None
     found_cg = cg.search({'search': 'tt'+str(m.ImdbId)})
     for t in m.Torrents:
         if 'Dead (participating in the contest)' in t.Trumpable:
@@ -30,16 +32,24 @@ def findByURL(cg, URL):
                 if sizeof_fmt(int(t.Size)) == cg_t['Size'] and cg_t['Seeders'] != '0':
                     print "Found possible match at %s (%s) with %s seeders" % (cg_t['Title'], cg_t['Size'], cg_t['Seeders'])
                     cg.downloadTorrent(cg_t['ID'], name=cg_t['Title']+'.torrent')
+    None
 
 def main():
     ptp = ptpapi.login()
     cg = cgapi.CGAPI()
     cg.login()
-    while True:
-        url = raw_input('url>>> ')
-        if url in ['q', 'quit', 'exit']:
-            break
-        findByURL(cg, url)
+    seen = pickle.load( open( 'seen.p', 'rb'))
+    with open(sys.argv[1], 'r') as fh:
+        for url in fh:
+            ptp_id = parse_qs(urlparse(URL).query)['id'][0]
+            if ptp_id in seen:
+                logger.error('Already seen movie %s' % ptp_id)
+                continue
+            else:
+                findByURL(cg, url)                
+                seen.append(ptp_id)
+                sleep(20)
+    pickle.dump( seen,  open( "seen.p", "wb" ) )
 
 if __name__ == '__main__':
     main()
