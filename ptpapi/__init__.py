b5e34feb00546e39cf6714c17a59b0fb3a74f896
kannibalox <kannibalox@gmail.com>
2018-04-09 17:35:53 +0000

    Doc fixup

--- ptpapi/__init__.py
+++ ptpapi/__init__.py
@@ -1,10 +1,9 @@
 # flake8: noqa
-"""Exists solely to make 'import api' possible"""
+"""Exists solely to make 'import ptpapi' possible"""
 from ptpapi.api import API
 from ptpapi.torrent import Torrent
 from ptpapi.movie import Movie
 from ptpapi.user import User
-
 
 def login(username=None, password=None, passkey=None):
     """A helper function to make it easy to log in"""
