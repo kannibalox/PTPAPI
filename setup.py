fc6b0418a3dba7a0840721412afa48a5c186cb45
kannibalox <kannibalox@gmail.com>
2018-06-28 17:39:28 +0000

    Move to a more normalized directory structure

--- setup.py
+++ setup.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="PTPAPI",
     version="0.4",
+    author="kannibalox",
+    url="https://github.com/kannibalox/PTPAPI",
     install_requires=[
         "requests",
         "beautifulsoup4",
-        "tempita"
+        "tempita",
+        "six"
     ],
     extras_require={
         'reseed': ['guessit>=2']
     },
-    packages=[
-        'ptpapi',
-        'ptpapi.sites',
-    ],
-    scripts=[
-        'scripts/ptp',
-        'scripts/ptp-reseed',
-        'scripts/ptp-reseed-machine',
-    ],
+    packages=find_packages('src'),
+    package_dir={'':'src'},
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'ptp=ptpapi.scripts.ptp:main',
+            'ptp-reseed=ptpapi.scripts.ptp_reseed:main',
+            'ptp-reseed-machine=ptpapi.scripts.ptp_reseed_machine:main',
+        ],
+    }
 )
