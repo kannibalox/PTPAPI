f3f7024401be23595a843a2b66ee1556bbc2959a
kannibalox <kannibalox@gmail.com>
2023-03-18 12:10:40 -0400

    Fix up archiver.sh and add tinycron

--- resources/archiver.sh
+++ resources/archiver.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-set -eu
-install_file="/data/archiver.py"
+set -u
+archive_script="/data/archiver.py"
 if [[ ! -d "/data/" ]]; then
   mkdir /data/
 fi
-if [[ ! -e "$install_file" ]]; then
-  curl -v -H "ApiUser: ${PTPAPI_APIUSER}" -H "ApiKey: ${PTPAPI_APIKEY}" "https://passthepopcorn.me/archive.php?action=script" \
-       > "$install_file"
+cd /data/ || exit 1 # Exit if /data/ isn't available
+if [[ ! -e "$archive_script" ]]; then
+  echo "archiver.py does not exist, installing"
+  wget --header "ApiUser: ${PTPAPI_APIUSER}" --header "ApiKey: ${PTPAPI_APIKEY}" \
+       "https://passthepopcorn.me/archive.php?action=script"  \
+       -O "$archive_script"
+  if [ $? -ne 0 ] ; then
+    echo "Could not download script"
+    if [[ -e "$archive_script" ]]; then
+      cat "$archive_script"
+      rm -f "$archive_script"
+    fi
+    exit
+  fi
 fi
-/venv/bin/python ${install_file}  "$@" --config /data/config.ptp
+/venv/bin/python "${archive_script}"  "$@"
